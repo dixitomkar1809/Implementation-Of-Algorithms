6f9e3777d365feabe95d1466af905bbf528186d5
dixitomkar1809 <dixitomkar1809@gmail.com>
2018-11-18 00:56:21 -0600

    SP 11 final

--- src/ond170030/SP11/kLargestElements.java
+++ src/ond170030/SP11/kLargestElements.java
@@ -1,14 +1,31 @@
 package ond170030.SP11;
 import java.util.PriorityQueue;
 
+/**
+ * Comparing o(n) select method for kth largest element with priority queue for stream of data
+ */
 public class kLargestElements {
 
     public static final int threshold = 2;
+
+    /**
+     * Select algorithm O(n)
+     * @param arr
+     * @param k
+     */
     public static void select(int[] arr, int k)
     {
         select(arr, 0, arr.length,k);
     }
 
+    /**
+     * Select Helper returns kth largest element
+     * @param arr
+     * @param p
+     * @param n
+     * @param k
+     * @return
+     */
     public static int select(int[] arr, int p, int n, int k)
     {
         //insertion sort
@@ -23,6 +40,13 @@ public class kLargestElements {
             return select(arr,p,left,k-right-1);
     }
 
+    /**
+     * Random partition returns a random pivot
+     * @param A
+     * @param p
+     * @param r
+     * @return
+     */
     static int randomPartition(int[] A, int p, int r)
     {
         int x=A[r];
@@ -44,6 +68,12 @@ public class kLargestElements {
         return i+1;
     }
 
+    /**
+     * returns kth largest element using priority queues
+     * @param arr
+     * @param k
+     * @return
+     */
     public static int selectPQ(int[] arr, int k)
     {
         PriorityQueue<Integer> q=new PriorityQueue<Integer>(k);
@@ -69,8 +99,13 @@ public class kLargestElements {
         return (int)temp[q.size()-1];
 //        return 0;
     }
+
+    /**
+     * Basic Driver code
+     * @param args
+     */
     public static void main(String[] args) {
-        int n = 32000000;
+        int n = 64000000;
         Timer timer1 = new Timer();
         Timer timer2 = new Timer();
         int[] arr = new int[n];
@@ -81,17 +116,19 @@ public class kLargestElements {
             arr[i] = i;
         }
         Shuffle.shuffle(arr);
-        int k= (int) Math.random() * range + min;
+//        int k= (int) Math.random() * range + min;
+        int k = n/2;
         timer1.start();
         selectPQ(arr,k);
         timer1.end();
-//        System.out.println("*************************");
         Shuffle.shuffle(arr);
         timer2.start();
         select(arr,k);
         timer2.end();
-        for(int i=0;i<k;i++)
+//        for(int i=0;i<k;i++)
 //            System.out.println(arr[arr.length-1-i]);
+        System.out.println("Value of n "+n);
+        System.out.println("Value of k "+k);
         System.out.println("PQ " + timer1.toString());
         System.out.println("Select " + timer2.toString());
 
