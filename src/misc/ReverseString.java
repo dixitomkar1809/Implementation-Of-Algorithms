41fde5c7b69b8c54d9d5dbf0d59f9090062b3a73
dixitomkar1809 <dixitomkar1809@gmail.com>
2018-12-09 22:10:25 -0600

    Added the reverse string

---
+++ src/misc/ReverseString.java
@@ -0,0 +1,11 @@
+package misc;
+
+public class ReverseString {
+
+    public static void main(String[] args){
+        String input = "Omkar";
+        StringBuilder sb = new StringBuilder();
+        sb.append(input);
+        System.out.println(sb.reverse());
+    }
+}
